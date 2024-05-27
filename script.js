ae9a2426240ec7464793997addd0fc201d0bd1c6
k-hain <155682079+k-hain@users.noreply.github.com>
2024-05-27 23:09:06 +0200

    Implement basic UI/button functionality

--- script.js
+++ script.js
@@ -1,3 +1,84 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+
+let operandA = '0';
+let operandB = '';
+let operator = '';
+
+const numberVals = '0123456789';
+const operatorVals = '+-*/';
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    processInput(button);
+  });
+});
+
+function processInput(button) {
+  if (numberVals.includes(button.id)) {
+    if (operator !== '') {
+      operandB = changeOperand(operandB, button);
+    } else {
+      operandA = changeOperand(operandA, button);
+    }
+  } else if (operatorVals.includes(button.id)) {
+    operator = button.id;
+  } else if (button.id === 'ce') {
+    if (operandB !== '') {
+      operandB = operandB.slice(0, -1);
+    } else if (operator !== '') {
+      operator = '';
+    } else {
+      if (operandA.length === 1) {
+        operandA = '0';
+      } else if (operandA.length === 2 && operandA.includes('-')) {
+        operandA = '0';
+      } else {
+        operandA = operandA.slice(0, -1);
+      }
+    }
+  } else if (button.id === '=') {
+    if (operandB.length >= 1) {
+      operandA = operate(operandA, operandB, operator);
+      operator = '';
+      operandB = '';
+    }
+  } else if (button.id === '.') {
+    if (operandB === '' && operator === '' && !operandA.includes('.')) {
+      operandA = changeOperand(operandA, button);
+    } else if (!operandB !== '' && !operandB.includes('.')) {
+      operandB = changeOperand(operandB, button);
+    }
+  }
+  updateDisplay();
+}
+
+function changeOperand(operand, button) {
+  if (operand === '0') {
+    return button.id;
+  } else {
+    return operand += button.id;
+  }
+}
+
+function updateDisplay() {
+  if (operandB === '' && operator === '') {
+    display.textContent = operandA;
+    console.log('Displaying A');
+  } else if (operandB === '') {
+    display.textContent = operandA + ' ' + operator
+    console.log('Displaying A+OP');
+  } else {
+    display.textContent = operandA + ' ' + operator + ' ' + operandB;
+    console.log('Displaying A+OP+B');
+  }
+  console.log('A: ' + operandA);
+  console.log('OP: ' + operator);
+  console.log('B: ' + operandB);
+  console.log(typeof operandA);
+  console.log('-----');
+}
+
 function add(a, b) {
 	return a + b;
 }
@@ -14,14 +95,14 @@ function multiply(a, b) {
 	return a * b;
 }
 
-function operate(a, b, operator) {
-  if (operator === '+') {
+function operate(a, b, operation) {
+  if (operation === '+') {
     return add(a, b);
-  } else if (operator === '-') {
-    return subtract(a, b);
-  } else if (operator === '/') {
-    return divide(a, b);
-  } else if (operator === '*') {
-    return multiply (a, b);
+  } else if (operation === '-') {
+    return subtract(a, b).toString();
+  } else if (operation === '/') {
+    return divide(a, b).toString();
+  } else if (operation === '*') {
+    return multiply(a, b).toString();
   }
 }
